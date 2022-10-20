e0f267b019aed573f0dd1fc8645ebedef45ab227
kayondomartin <kayondomartin45@gmail.com>
2022-10-20 16:47:47 +0900

    Delete elpa directory

--- elpa/font-lock-profiler-20170208.2008/font-lock-profiler-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from font-lock-profiler-20170208.2008/font-lock-profiler.el  -*- no-byte-compile: t -*-
-(define-package "font-lock-profiler" "20170208.2008" "Coverage and timing tool for font-lock keywords." '((emacs "24.3")) :commit "6e096458416888a4f63cca0d6bc5965a052753c8" :authors '(("Anders Lindgren")) :maintainer '("Anders Lindgren") :keywords '("faces" "tools") :url "https://github.com/Lindydancer/font-lock-profiler")
